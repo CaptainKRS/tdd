86d18d0d7b96d7345677b1ededf44a95b0d7fecd
Kirk Sullivan <kirk_135@hotmail.com>
2018-08-19 14:34:26 -0400

    Added state testing with a button onClick counter that increments correctly

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,14 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * Factory function to create a ShallowWrapper for the App component
  * @function setup
  * @param {object} props - Component props specific to this setup.
- * @param {any} state - Initial state for setup.
+ * @param {object} state - Initial state for setup.
  * @returns {ShallowWrapper}
  */
 
 const setup = (props = {}, state = null) => {
-  return shallow(<App {...props} />);
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
 };
 
 /**
@@ -48,8 +50,21 @@ test('renders counter display', () => {
 
 test('counter starts at 0', () => {
   //test the state
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
 });
 
 test('clicking button increments counter display', () => {
-  //testing the display is the furthest removed from the implementation
+  const counter = 7;
+  const wrapper = setup(null,{counter});
+
+  // find button and click
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  wrapper.update();
+
+  // find display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1)
 });
